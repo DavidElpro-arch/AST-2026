e95b0edbbfd064a812c118eede2f97cd7bb433aa
david.algarra <david.algarra@telem3-3>
2026-09-21 11:02:05 +0200

    hola

---
+++ src/practica1/Protocol/TSocketSend.java
@@ -0,0 +1,21 @@
+package practica1.Protocol;
+
+import util.TCPSegment;
+import util.TSocket_base;
+import util.SimNet;
+
+public class TSocketSend extends TSocket_base {
+
+  public TSocketSend(SimNet network) {
+    super(network);
+  }
+
+  @Override
+  public void sendData(byte[] data, int offset, int length) {
+    TCPSegment segment = new TCPSegment();
+    segment.setData(data, offset, length);
+    segment.setPsh(true);
+    SimNet.send(segment);
+    
+  }
+}
